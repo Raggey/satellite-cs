1e99136b4f4b414e2d680a9a104d1d10d6560f56
Craig Robinson <raggy1995@gmail.com>
2020-09-14 20:21:40 +1000

    Tested with ISS, matching lat,long

--- src/Program.cs
+++ src/Program.cs
@@ -10,8 +10,8 @@ namespace Satellite_cs
 
 
         // string line0 = "ISS (ZARYA)    ";
-        string line1 = "1 25544U 98067A   20206.38292522 -.00000985  00000-0 -95291-5 0  9998";
-        string line2 = "2 25544  51.6430 164.3636 0001088 140.8410 323.1994 15.49511774237787";
+        string line1 = "1 25544U 98067A   20258.18510567  .00000068  00000-0  93716-5 0  9995";
+        string line2 = "2 25544  51.6438 268.0523 0000907  94.9123  25.5188 15.48946285245810";
         
         Sat_Io io = new Sat_Io();
         Satrec satrec = io.twoline2satrec(line1,line2);
@@ -57,10 +57,32 @@ namespace Satellite_cs
         LookAngles lookAngles = tf.ecfToLookAngles(observerGd, positionEcf);
         double dopplerFactor = df.dopplerFactor(observerEcf, positionEcf, velocityEcf);
 
-        // Console.WriteLine(dopplerFactor);
-        // Console.WriteLine(lookAngles.azimuth);
-        // Console.WriteLine(lookAngles.elevation);
-        // Console.WriteLine(lookAngles.rangeSat);
+        // The position_velocity result is a key-value pair of ECI coordinates.
+        // These are the base results from which all other coordinates are derived.
+        Coordinates positionEci = positionAndVelocity.position_ECI;
+        Coordinates velocityEci = positionAndVelocity.velocity_ECI;
+
+        double satelliteX = positionEci.x;
+        double satelliteY = positionEci.y;
+        double satelliteZ = positionEci.z;
+
+         // Look Angles may be accessed by `azimuth`, `elevation`, `range_sat` properties.
+        double azimuth = lookAngles.azimuth;
+        double elevation = lookAngles.elevation;
+        double rangeSat  = lookAngles.rangeSat;
+
+        // Geodetic coords are accessed via `longitude`, `latitude`, `height`.
+        double longitude = positionGd.longitude;
+        double latitude  = positionGd.latitude;
+        double height    = positionGd.height;
+
+        //  Convert the RADIANS to DEGREES for pretty printing (appends "N", "S", "E", "W", etc).
+        double longitudeStr = tf.degreesLong(longitude);
+        double latitudeStr  = tf.degreesLat(latitude);
+
+        Console.WriteLine("Long: " + longitudeStr);
+        Console.WriteLine("Lat: " + latitudeStr);
+
         Console.WriteLine("Look i didnt crash");
 
 
